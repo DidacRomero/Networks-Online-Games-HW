d5bbf460d8f407b0778a05a3e1db58c23f42123d
Dídac Romero <36265669+DidacRomero@users.noreply.github.com>
2020-10-18 17:18:07 +0200

    Initialized Socckets lib!

--- Sockets/UDP Server/Main.cpp
+++ Sockets/UDP Server/Main.cpp
@@ -1,7 +1,14 @@
-//#include <stdio.h>
+﻿//#include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+
+
+//Sockets includes
+#define WIN_LEAN_AND_MEAN
+#define NOMINMAX
+#include <WinSock2.h>
 #include <Windows.h>
+#include <Ws2tcpip.h>
 
 void bind()
 {
@@ -24,11 +31,30 @@ void closeSocket()
 }
 
 
+bool initializeSocketsLib()
+{
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	if (iResult != NO_ERROR)
+	{
+		std::cout << "Server initialization: FAILED \n";
+		std::cout << "----------\n";
+		return false;
+	}
+	std::cout << "Server initialization: SUCCESS \n";
+	std::cout << "----------\n";
+	return true;
+}
+
+
 
 //UDP CLIENT Entry Point
 int main(int argc, char** argv)
 {
-	std::cout << "UDP Server Start! \n";
+	std::cout << "------------------------------------------------- UDP SERVER Start! ------------------------------------------------- \n";
+	//Initialize the Sockets library
+	initializeSocketsLib();
 
 	for (int i = 0; i < 5; ++i)
 	{
--- Sockets/UDP_Client/Main.cpp
+++ Sockets/UDP_Client/Main.cpp
@@ -1,9 +1,13 @@
 //#include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+
+//Sockets includes
+#define WIN_LEAN_AND_MEAN
+#define NOMINMAX
+#include <WinSock2.h>
 #include <Windows.h>
-
-
+#include <Ws2tcpip.h>
 
 void sendTo()
 {
@@ -19,10 +23,32 @@ void closeSocket()
 {
 
 }
+
+bool initializeSocketsLib()
+{
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	if (iResult != NO_ERROR)
+	{
+		//Initialization FAILURE
+		std::cout << "Server initialization: FAILED \n";
+		std::cout << "----------\n";
+		return false;
+	}
+
+	// Initialization SUCCESS
+	std::cout << "Server initialization: SUCCESS \n";
+	std::cout << "----------\n";
+	return true;
+}
+
 //UDP CLIENT Entry Point
 int main(int argc, char **argv)
 {
-	std::cout << "UDP Client Start! \n";
+	std::cout << "------------------------------------------------- UDP CLIENT Start! ----------------------------------------------------\n";
+	//Initialize Sockets
+	initializeSocketsLib();
 
 	for (int i = 0; i < 5; ++i)
 	{
